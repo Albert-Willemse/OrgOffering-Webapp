d44ddca0435e29c70bd3fa7842f0033370dbccac
ALBERT WILLEMSE <38205742@student365.msfed.nwu.ac.za>
2023-09-14 11:56:10 +0200

    Add Azure Secrets

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace OrgOffering
 {
@@ -18,6 +19,11 @@ namespace OrgOffering
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("ConnectionStrings:DefaultConnection"));
+config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
